5f35aa1e9d32eb4642f50cdd5dba636c598ecb3f
jamocity <jamiuokunlola202@gmail.com>
2023-03-05 09:51:55 +0100

    queryDB extention as changed

---
+++ queryDB.mjs
@@ -0,0 +1,13 @@
+import fs from "fs";
+import {exit} from "process";
+
+export default async function queryDB(externalFunction){
+fs.readFile("dbFileCheck.js", function(err, data){
+	     if(err){
+	     	console.log("somthing went wrong", err);
+	     	exit(1);
+	     }
+	     console.log(data.toString());
+});
+}
+queryDB();
