c008e839e2c9ea35596cecd90115c1d5e43d9de9
jamocity <jamiuokunlola202@gmail.com>
2023-03-06 09:11:37 +0100

    queryDb is now fetching db.jon file

--- queryDB.js
+++ queryDB.js
@@ -1,9 +1,12 @@
 var fs=require ("fs");
 var exit=require("process");
+export default async function queryDB(externalFunction){
 fs.readFile("dbFileCheck.js", function(err, data){
 	     if(err){
 	     	console.log("somthing went wrong", err);
 	     	exit(1);
 	     }
 	     console.log(data.toString());
-})
+});
+}
+queryDB();
